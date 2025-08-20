3839fbd097ae6e97d7943d57aa20b18ed61b79bc
pinyapat55 <pinyapat.so@ku.th>
2025-08-21 02:01:03 +0700

    Updates

---
+++ precache-manifest.4dccd8c9ae02a033b2db38c68b7f8053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81063464a1cd11e834f7f3760665b4ff",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "01133bc37e94e78c5721",
+    "url": "/expense-tracker-react/static/css/main.e62be5bb.chunk.css"
+  },
+  {
+    "revision": "3bf5b622c34b4cbf088f",
+    "url": "/expense-tracker-react/static/js/2.5b36f217.chunk.js"
+  },
+  {
+    "revision": "006f0232cb3dc8d9eae501e897917de3",
+    "url": "/expense-tracker-react/static/js/2.5b36f217.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01133bc37e94e78c5721",
+    "url": "/expense-tracker-react/static/js/main.af08a7f6.chunk.js"
+  },
+  {
+    "revision": "17b918ff1411e241436c",
+    "url": "/expense-tracker-react/static/js/runtime-main.c2a1a911.js"
+  }
+]);
